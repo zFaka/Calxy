873dabda601e37903934cbd35942b66e40eee860
zFaka <fakakill2000@gmail.com>
2021-01-28 12:58:24 -0300

    upload heroku

---
+++ Back/public/precache-manifest.9d907e088272c067ce421406dae8604a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2576b96d5818af69b24b3eb4c6bf084",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c5cd7f0eee8eb868634",
+    "url": "/static/css/2.ee20335c.chunk.css"
+  },
+  {
+    "revision": "d535d36e2b4e28ba2e92",
+    "url": "/static/css/main.87c3682f.chunk.css"
+  },
+  {
+    "revision": "8c5cd7f0eee8eb868634",
+    "url": "/static/js/2.bd64b146.chunk.js"
+  },
+  {
+    "revision": "fa4b4056bba234820139274405351863",
+    "url": "/static/js/2.bd64b146.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d535d36e2b4e28ba2e92",
+    "url": "/static/js/main.bb3d1444.chunk.js"
+  },
+  {
+    "revision": "c2ab9c45931d577cf16e",
+    "url": "/static/js/runtime-main.27b01e2e.js"
+  }
+]);
